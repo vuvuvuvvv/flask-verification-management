62f810c7261cc9096a6052e85801973eff39d16e
vuvuvuvvv <nguyenvu260502@gmail.com>
2024-09-25 17:29:00 +0700

    feat: update more func for filter & create dongho+change: change name column and add more column to dongho table

--- app/models.py
+++ app/models.py
@@ -3,11 +3,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 
+
 class User(UserMixin, db.Model):
-    __tablename__ = 'user'
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
-    fullname = db.Column(db.String(100), index=True, nullable=False, default="404 Notfound")
+    fullname = db.Column(
+        db.String(100), index=True, nullable=False, default="404 Notfound"
+    )
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(10), default="USER")
@@ -26,15 +29,16 @@ class User(UserMixin, db.Model):
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'username': self.username,
-            'fullname': self.fullname,
-            'email': self.email,
-            'role': self.role
+            "id": self.id,
+            "username": self.username,
+            "fullname": self.fullname,
+            "email": self.email,
+            "role": self.role,
         }
 
+
 class TokenBlacklist(db.Model):
-    __tablename__ = 'token_blacklist'
+    __tablename__ = "token_blacklist"
     id = db.Column(db.Integer, primary_key=True)
     jti = db.Column(db.String(36), nullable=False, unique=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
@@ -46,8 +50,8 @@ class TokenBlacklist(db.Model):
 
 
 class PDM(db.Model):
-    __tablename__ = 'pdm'
-    
+    __tablename__ = "pdm"
+
     id = db.Column(db.Integer, primary_key=True)
     ma_tim_dong_ho_pdm = db.Column(db.String(255), nullable=True)
     ten_dong_ho = db.Column(db.String(255), nullable=False)
@@ -66,7 +70,25 @@ class PDM(db.Model):
     ngay_het_han = db.Column(db.DateTime, nullable=True)
     anh_pdm = db.Column(db.String(255), nullable=True)
 
-    def __init__(self, ma_tim_dong_ho_pdm, ten_dong_ho, noi_san_xuat, dn, ccx, kieu_sensor, transmitter, qn, q3, r, don_vi_pdm, dia_chi, so_qd_pdm, ngay_qd_pdm, ngay_het_han, anh_pdm):
+    def __init__(
+        self,
+        ma_tim_dong_ho_pdm,
+        ten_dong_ho,
+        noi_san_xuat,
+        dn,
+        ccx,
+        kieu_sensor,
+        transmitter,
+        qn,
+        q3,
+        r,
+        don_vi_pdm,
+        dia_chi,
+        so_qd_pdm,
+        ngay_qd_pdm,
+        ngay_het_han,
+        anh_pdm,
+    ):
         self.ma_tim_dong_ho_pdm = ma_tim_dong_ho_pdm
         self.ten_dong_ho = ten_dong_ho
         self.noi_san_xuat = noi_san_xuat
@@ -86,30 +108,32 @@ class PDM(db.Model):
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'ma_tim_dong_ho_pdm': self.ma_tim_dong_ho_pdm,
-            'ten_dong_ho': self.ten_dong_ho,
-            'noi_san_xuat': self.noi_san_xuat,
-            'dn': self.dn,
-            'ccx': self.ccx,
-            'kieu_sensor': self.kieu_sensor,
-            'transmitter': self.transmitter,
-            'qn': self.qn,
-            'q3': self.q3,
-            'r': self.r,
-            'don_vi_pdm': self.don_vi_pdm,
-            'dia_chi': self.dia_chi,
-            'so_qd_pdm': self.so_qd_pdm,
-            'ngay_qd_pdm': self.ngay_qd_pdm,
-            'ngay_het_han': self.ngay_het_han,
-            'anh_pdm': self.anh_pdm
+            "id": self.id,
+            "ma_tim_dong_ho_pdm": self.ma_tim_dong_ho_pdm,
+            "ten_dong_ho": self.ten_dong_ho,
+            "noi_san_xuat": self.noi_san_xuat,
+            "dn": self.dn,
+            "ccx": self.ccx,
+            "kieu_sensor": self.kieu_sensor,
+            "transmitter": self.transmitter,
+            "qn": self.qn,
+            "q3": self.q3,
+            "r": self.r,
+            "don_vi_pdm": self.don_vi_pdm,
+            "dia_chi": self.dia_chi,
+            "so_qd_pdm": self.so_qd_pdm,
+            "ngay_qd_pdm": self.ngay_qd_pdm,
+            "ngay_het_han": self.ngay_het_han,
+            "anh_pdm": self.anh_pdm,
         }
 
 
 class DongHo(db.Model):
-    __tablename__ = 'dongho'
-    
-    seri_number = db.Column(db.String(255), primary_key=True)  # Sử dụng seri_number làm khóa chính
+    __tablename__ = "dongho"
+
+    serial_number = db.Column(
+        db.String(255), primary_key=True
+    )  # Sử dụng serial_number làm khóa chính
     phuong_tien_do = db.Column(db.String(255), nullable=True)
     seri_chi_thi = db.Column(db.String(255), nullable=True)
     seri_sensor = db.Column(db.String(255), nullable=True)
@@ -131,15 +155,49 @@ class DongHo(db.Model):
     co_so_su_dung = db.Column(db.String(255), nullable=True)
     phuong_phap_thuc_hien = db.Column(db.String(255), nullable=True)
     chuan_thiet_bi_su_dung = db.Column(db.String(255), nullable=True)
-    implementer = db.Column(db.String(255), nullable=True)
+    nguoi_kiem_dinh = db.Column(db.String(255), nullable=True)
     ngay_thuc_hien = db.Column(db.Date, nullable=True)
+    hieu_luc_bien_ban = db.Column(db.Date, nullable=True)
     vi_tri = db.Column(db.String(255), nullable=True)
     nhiet_do = db.Column(db.String(255), nullable=True)
     do_am = db.Column(db.String(255), nullable=True)
-    du_lieu_kieu_dinh = db.Column(db.Text, nullable=True)
+    du_lieu_kiem_dinh = db.Column(db.Text, nullable=True)
+    so_giay_chung_nhan = db.Column(db.String(255), nullable=True)
 
-    def __init__(self, seri_number, phuong_tien_do, seri_chi_thi, seri_sensor, kieu_chi_thi, kieu_sensor, kieu_thiet_bi, co_so_san_xuat, so_tem, nam_san_xuat, dn, d, ccx, q3, r, qn, k_factor, so_qd_pdm, ten_khach_hang, co_so_su_dung, phuong_phap_thuc_hien, chuan_thiet_bi_su_dung, implementer, ngay_thuc_hien, vi_tri, nhiet_do, do_am, du_lieu_kieu_dinh):
-        self.seri_number = seri_number
+    def __init__(
+        self,
+        serial_number,
+        phuong_tien_do,
+        seri_chi_thi,
+        seri_sensor,
+        kieu_chi_thi,
+        kieu_sensor,
+        kieu_thiet_bi,
+        co_so_san_xuat,
+        so_tem,
+        nam_san_xuat,
+        dn,
+        d,
+        ccx,
+        q3,
+        r,
+        qn,
+        k_factor,
+        so_qd_pdm,
+        ten_khach_hang,
+        co_so_su_dung,
+        phuong_phap_thuc_hien,
+        chuan_thiet_bi_su_dung,
+        nguoi_kiem_dinh,
+        ngay_thuc_hien,
+        vi_tri,
+        nhiet_do,
+        do_am,
+        du_lieu_kiem_dinh,
+        hieu_luc_bien_ban,
+        so_giay_chung_nhan,
+    ):
+        self.serial_number = serial_number
         self.phuong_tien_do = phuong_tien_do
         self.seri_chi_thi = seri_chi_thi
         self.seri_sensor = seri_sensor
@@ -161,41 +219,45 @@ class DongHo(db.Model):
         self.co_so_su_dung = co_so_su_dung
         self.phuong_phap_thuc_hien = phuong_phap_thuc_hien
         self.chuan_thiet_bi_su_dung = chuan_thiet_bi_su_dung
-        self.implementer = implementer
+        self.nguoi_kiem_dinh = nguoi_kiem_dinh
         self.ngay_thuc_hien = ngay_thuc_hien
         self.vi_tri = vi_tri
         self.nhiet_do = nhiet_do
         self.do_am = do_am
-        self.du_lieu_kieu_dinh = du_lieu_kieu_dinh
+        self.du_lieu_kiem_dinh = du_lieu_kiem_dinh
+        self.hieu_luc_bien_ban = hieu_luc_bien_ban
+        self.so_giay_chung_nhan = so_giay_chung_nhan
 
     def to_dict(self):
         return {
-            'seri_number': self.seri_number,
-            'phuong_tien_do': self.phuong_tien_do,
-            'seri_chi_thi': self.seri_chi_thi,
-            'seri_sensor': self.seri_sensor,
-            'kieu_chi_thi': self.kieu_chi_thi,
-            'kieu_sensor': self.kieu_sensor,
-            'kieu_thiet_bi': self.kieu_thiet_bi,
-            'co_so_san_xuat': self.co_so_san_xuat,
-            'so_tem': self.so_tem,
-            'nam_san_xuat': self.nam_san_xuat,
-            'dn': self.dn,
-            'd': self.d,
-            'ccx': self.ccx,
-            'q3': self.q3,
-            'r': self.r,
-            'qn': self.qn,
-            'k_factor': self.k_factor,
-            'so_qd_pdm': self.so_qd_pdm,
-            'ten_khach_hang': self.ten_khach_hang,
-            'co_so_su_dung': self.co_so_su_dung,
-            'phuong_phap_thuc_hien': self.phuong_phap_thuc_hien,
-            'chuan_thiet_bi_su_dung': self.chuan_thiet_bi_su_dung,
-            'implementer': self.implementer,
-            'ngay_thuc_hien': self.ngay_thuc_hien,
-            'vi_tri': self.vi_tri,
-            'nhiet_do': self.nhiet_do,
-            'do_am': self.do_am,
-            'du_lieu_kieu_dinh': self.du_lieu_kieu_dinh
+            "serial_number": self.serial_number,
+            "phuong_tien_do": self.phuong_tien_do,
+            "seri_chi_thi": self.seri_chi_thi,
+            "seri_sensor": self.seri_sensor,
+            "kieu_chi_thi": self.kieu_chi_thi,
+            "kieu_sensor": self.kieu_sensor,
+            "kieu_thiet_bi": self.kieu_thiet_bi,
+            "co_so_san_xuat": self.co_so_san_xuat,
+            "so_tem": self.so_tem,
+            "nam_san_xuat": self.nam_san_xuat,
+            "dn": self.dn,
+            "d": self.d,
+            "ccx": self.ccx,
+            "q3": self.q3,
+            "r": self.r,
+            "qn": self.qn,
+            "k_factor": self.k_factor,
+            "so_qd_pdm": self.so_qd_pdm,
+            "ten_khach_hang": self.ten_khach_hang,
+            "co_so_su_dung": self.co_so_su_dung,
+            "phuong_phap_thuc_hien": self.phuong_phap_thuc_hien,
+            "chuan_thiet_bi_su_dung": self.chuan_thiet_bi_su_dung,
+            "nguoi_kiem_dinh": self.nguoi_kiem_dinh,
+            "ngay_thuc_hien": self.ngay_thuc_hien,
+            "vi_tri": self.vi_tri,
+            "nhiet_do": self.nhiet_do,
+            "do_am": self.do_am,
+            "du_lieu_kiem_dinh": self.du_lieu_kiem_dinh,
+            "hieu_luc_bien_ban": self.hieu_luc_bien_ban,
+            "so_giay_chung_nhan": self.so_giay_chung_nhan,
         }
--- app/routes/api/dongho.py
+++ app/routes/api/dongho.py
@@ -2,52 +2,117 @@ from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
 from app.models import DongHo
 from app import db
+import json
 
-dongho_bp = Blueprint('dongho', __name__)
+dongho_bp = Blueprint("dongho", __name__)
 
-@dongho_bp.route('', methods=['POST'])
+
+@dongho_bp.route("", methods=["GET"])
+@jwt_required()
+def get_donghos():
+    try:
+        query = DongHo.query
+
+        so_giay_chung_nhan = request.args.get("so_giay_chung_nhan")
+        if so_giay_chung_nhan:
+            query = query.filter(DongHo.so_giay_chung_nhan.ilike(f"%{so_giay_chung_nhan}%"))
+
+        ten_khach_hang = request.args.get("ten_khach_hang")
+        if ten_khach_hang:
+            query = query.filter(DongHo.ten_khach_hang.ilike(f"%{ten_khach_hang}%"))
+
+        nguoi_kiem_dinh = request.args.get("nguoi_kiem_dinh")
+        if nguoi_kiem_dinh:
+            query = query.filter(DongHo.nguoi_kiem_dinh.ilike(f"%{nguoi_kiem_dinh}%"))
+
+        ngay_kiem_dinh_from = request.args.get("ngay_kiem_dinh_from")
+        if ngay_kiem_dinh_from:
+            query = query.filter(DongHo.ngay_qd_pdm >= ngay_kiem_dinh_from)
+
+        ngay_kiem_dinh_to = request.args.get("ngay_kiem_dinh_to")
+        if ngay_kiem_dinh_to:
+            query = query.filter(DongHo.ngay_qd_pdm <= ngay_kiem_dinh_to)
+
+        donghos = query.all()
+        
+        result = []
+        for dongho in donghos:
+            dongho_dict = dongho.to_dict()
+            if "du_lieu_kiem_dinh" in dongho_dict:
+                try:
+                    dongho_dict["du_lieu_kiem_dinh"] = json.loads(
+                        dongho_dict["du_lieu_kiem_dinh"]
+                    )
+                except json.JSONDecodeError as e:
+                    return jsonify({"msg": f"JSON decode error: {str(e)}"}), 500
+            result.append(dongho_dict)
+        return jsonify(result), 200
+    except Exception as e:
+        return jsonify({"msg": f"Đã xảy ra lỗi: {str(e)}"}), 500
+
+@dongho_bp.route("", methods=["POST"])
 @jwt_required()
 def create_dongho():
-    data = request.get_json()
-    seri_number = data.get('seri_number')
+    try:
+        data = request.get_json()
+        serial_number = data.get("serial_number")
+        # Check if serial_number already exists
+        existing_dongho = DongHo.query.filter_by(serial_number=serial_number).first()
+        if existing_dongho:
+            return jsonify({"msg": "Serial number đã tồn tại!"}), 400
+        new_dongho = DongHo(**data)
+        db.session.add(new_dongho)
+        db.session.commit()
+        return jsonify(new_dongho.to_dict()), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"msg": f"Đã xảy ra lỗi: {str(e)}"}), 500
 
-    # Check if seri_number already exists
-    existing_dongho = DongHo.query.filter_by(seri_number=seri_number).first()
-    if existing_dongho:
-        return jsonify({"msg": "Serinumber đã tồn tại!"}), 400
 
-    new_dongho = DongHo(**data)
-    db.session.add(new_dongho)
-    db.session.commit()
-    return jsonify(new_dongho.to_dict()), 201
-
-@dongho_bp.route('/<int:id>', methods=['PUT'])
+@dongho_bp.route("/<int:id>", methods=["PUT"])
 @jwt_required()
 def update_dongho(id):
-    data = request.get_json()
-    dongho = DongHo.query.get_or_404(id)
-    for key, value in data.items():
-        setattr(dongho, key, value)
-    db.session.commit()
-    return jsonify(dongho.to_dict()), 200
+    try:
+        data = request.get_json()
+        dongho = DongHo.query.get_or_404(id)
+        for key, value in data.items():
+            setattr(dongho, key, value)
+        db.session.commit()
+        return jsonify(dongho.to_dict()), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"msg": f"Đã xảy ra lỗi: {str(e)}"}), 500
 
-@dongho_bp.route('/<int:id>', methods=['DELETE'])
-@jwt_required()
-def delete_dongho(id):
-    dongho = DongHo.query.get_or_404(id)
-    db.session.delete(dongho)
-    db.session.commit()
-    return jsonify({"msg": "Xóa thành công!"}), 200
 
-@dongho_bp.route('/serinumber/<string:serinumber>', methods=['GET'])
+@dongho_bp.route("/<string:serial_number>", methods=["DELETE"])
 @jwt_required()
-def get_dongho_by_serinumber(serinumber):
-    dongho = DongHo.query.filter_by(serinumber=serinumber).first_or_404()
-    return jsonify(dongho.to_dict()), 200
+def delete_dongho(serial_number):
+    try:
+        dongho = DongHo.query.get_or_404(serial_number)
+        db.session.delete(dongho)
+        db.session.commit()
+        return jsonify({"msg": "Xóa thành công!"}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"msg": f"Đã xảy ra lỗi: {str(e)}"}), 500
 
-@dongho_bp.route('/tenkhachhang/<string:tenkhachhang>', methods=['GET'])
+
+@dongho_bp.route("/serial-number/<string:serial_number>", methods=["GET"])
 @jwt_required()
-def get_dongho_by_tenkhachhang(tenkhachhang):
-    donghos = DongHo.query.filter_by(tenkhachhang=tenkhachhang).all()
-    result = [dongho.to_dict() for dongho in donghos]
-    return jsonify(result), 200
+def get_dongho_by_serial_number(serial_number):
+    try:
+        dongho = DongHo.query.filter_by(serial_number=serial_number).first_or_404()
+        return jsonify(dongho.to_dict()), 200
+    except Exception as e:
+        return jsonify({"msg": f"Đã xảy ra lỗi: {str(e)}"}), 500
+
+
+@dongho_bp.route("/ten-khach-hang/<string:ten_khach_hang>", methods=["GET"])
+@jwt_required()
+def get_dongho_by_ten_khach_hang(ten_khach_hang):
+    try:
+        donghos = DongHo.query.filter_by(ten_khach_hang=ten_khach_hang).all()
+        result = [dongho.to_dict() for dongho in donghos]
+        return jsonify(result), 200
+    except Exception as e:
+        return jsonify({"msg": f"Đã xảy ra lỗi: {str(e)}"}), 500
